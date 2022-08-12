e432c3189da8d9106ec35074d20058ae235e2481
0rque <shchuk1naaams@gmail.com>
2022-08-12 11:11:08 +0400

    init commit

---
+++ src/test/java/com/vms/fleetapp/VehicleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vms.fleetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
